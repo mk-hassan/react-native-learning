bd11e11fc388c6ce5511e4770dad5884e1261d05
Mahmoud Kamal <mk.bayumi@gmail.com>
2024-06-22 02:25:21 +0300

    new start

--- App.js
+++ App.js
@@ -3,50 +3,7 @@ import icon from "./assets/adaptive-icon.png";
 import { useState } from "react";
 
 export default function App() {
-  const [modalVisibility, setModalVisibility] = useState(false);
-
   return (
-    <View style={styles.container}>
-      <View style={[styles.box, styles.lightBox, styles.boxShadow]}>
-        <Text style={styles.title}>Welcome</Text>
-      </View>
-      <View style={[styles.box, styles.darkBox]}>
-        <Text style={styles.title}>Welcome</Text>
-      </View>
-    </View>
+    null
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 50,
-    flex: 1,
-    backgroundColor: "midnightblue"
-  },
-  box: {
-    width: 300,
-    height: 200,
-    padding: 10,
-    margin: 30,
-  },
-  lightBox: {
-    backgroundColor: "white",
-  },
-  darkBox: {
-    backgroundColor: "black",
-  },
-  title: {
-    color: "gray",
-    fontSize: 50,
-    fontWeight: "bolder"
-  },
-  boxShadow: {
-    shadowColor: "orange",
-    shadowOffset: {
-      width: 300,
-      height: 10,
-    },
-    shadowOpacity: 1,
-    shadowRadius: 2
-  }
-});
