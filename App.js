5a8e96c3f0c43a21c43e19a60a744967a795d164
Mahmoud Kamal <mk.bayumi@gmail.com>
2024-06-25 05:11:36 +0300

    cleaning up

--- App.js
+++ App.js
@@ -1,33 +1,5 @@
-import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
-
-import PokemonCard from "./components/PokemonCard";
-import charactersData from './data/pokemonCards.json';
-
 export default function App() {
   return (
-    <SafeAreaView style={styles.safeAreaContainer}>
-      <ScrollView style={styles.cardsContainer} contentContainerStyle={{ gap: 20 }}>
-        {
-          charactersData.map(cardData =>
-            <PokemonCard key={cardData.name} {...cardData}>
-              <PokemonCard.Title name={cardData.name} hp={cardData.hp} />
-              <PokemonCard.CharacterImage {...cardData} />
-              <PokemonCard.CharacterInfo {...cardData} />
-            </PokemonCard>
-          )
-        }
-      </ScrollView>
-    </SafeAreaView>
+    null
   );
 }
-
-const styles = StyleSheet.create({
-  safeAreaContainer: {
-    flex: 1,
-    backgroundColor: "#ede",
-  },
-  cardsContainer: {
-    gap: 10,
-    paddingHorizontal: 20,
-  }
-});
--- modules/characterImagesLoader.js
+++
@@ -1,6 +0,0 @@
-export default {
-  Charmander: require("../assets/pokemonImages/charmander.png"),
-  Bulbasaur: require("../assets/pokemonImages/bulbasaur.png"),
-  Pikachu: require("../assets/pokemonImages/pikachu.png"),
-  Squirtle: require("../assets/pokemonImages/squirtle.png"),
-}
